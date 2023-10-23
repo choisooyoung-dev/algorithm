c870ab0fedd35d24b1b39b3bc87ffcb3a5f5fbdd
sooyoung choi <sooyoung.choi.dev@gmail.com>
2023-10-23 18:08:09 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,5 +1,1 @@
-const solution = (slice, n) => {
-    if(n <= slice || n <= 1) return 1;
-    else if(n % slice !== 0) return Math.floor(n / slice) + 1;
-    else return n / slice;
-}
+const solution = (slice, n) => Math.ceil(n / slice);
