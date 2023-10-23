e899ac933e49cd46c08c302e2cd0495e72d33a68
sooyoung choi <sooyoung.choi.dev@gmail.com>
2023-10-23 19:31:52 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -2,7 +2,6 @@ function solution(sides) {
     
     sides.sort((a, b) => a - b);
     
-    if(sides[0] + sides[1] > sides[2]) return 1;
-    else return 2;
+    return sides[0] + sides[1] > sides[2] ? 1 : 2;
         
 }
