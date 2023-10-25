51354e50847ef95bd6128d5d224687cd8b2b541d
sooyoung choi <sooyoung.choi.dev@gmail.com>
2023-10-25 11:54:36 +0900

    [level unrated] Title: 암호 해독, Time: 5.45 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,12 @@
+function solution(cipher, code) {
+    var answer = '';
+    let strArr = [...cipher];
+
+    for(let i = 1; i <=cipher.length; i++) {
+        if(i % code === 0) {
+        console.log(cipher[i-1])
+        answer += cipher[i-1]
+        }
+    }
+    return answer;
+}
