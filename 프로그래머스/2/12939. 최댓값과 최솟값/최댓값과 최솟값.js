1237cb601fe0b0f48e6d7331b40ec38f342b91cd
sooyoung choi <sooyoung.choi.dev@gmail.com>
2023-12-28 14:26:12 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 2.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,9 +1,4 @@
 function solution(s) {
     var answer = '';
-    const splitStrArr = s.split(' ');
-    const maxNum = Math.max(...splitStrArr);
-    const minNum = Math.min(...splitStrArr);
-    answer = minNum + " " + maxNum
-    console.log(answer)
     return answer;
 }
