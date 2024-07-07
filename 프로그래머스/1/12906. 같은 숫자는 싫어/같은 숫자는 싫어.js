dfa1a1ece080b39c46804bfe2d3d96738fda8586
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2024-07-07 15:00:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.87 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr){
+    const newArr = [];
+    
+    for(i = 0; i < arr.length; i++){
+        if(arr[i] !== arr[i+1]){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
